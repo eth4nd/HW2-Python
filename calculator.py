6f130e3eafc83f3ca0dbef98d7594376072c4ff2
Ethan <ethan@MacBook-Pro.hsd1.ca.comcast.net>
2021-03-02 20:15:04 -0800

    More changes to calculator.py and timing.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,29 @@
 def calculator(number1, number2, operator):
     if operator == '+':
-       print(number1 + number 2)
+       return (number1 + number 2)
     elif operator == '-':
-       print(number1 - number 2)
+       return (number1 - number 2)
     elif operator == '*':
-       print(number1 * number 2)
+       return (number1 * number 2)
     elif operator == '/':
-       print(number1 / number 2)
+       return (number1 / number 2)
     elif operator == '//':
-       print(number1 // number 2)
+       return (number1 // number 2)
     elif operator == '**':
-       print(number1 ** number 2)
+       return (number1 ** number 2)
     else:
        quit()
 
 def input_output():
     number1 = input("Enter the first number: ")
-    print(int(number1))
+    return (int(number1))
     number2 = input("Enter the second number: ")
-    print(int(number2))
+    return (int(number2))
     operator = input("Enter the operation: ")
-    print(operator)
+    return (operator)
     calculator(number1, number2, operator)
     continue_q = input("Do you wish to exit? ")
-    print(continue_q)
+    return (continue_q)
     if continue_q == 'n':
        input_output()
     else:
