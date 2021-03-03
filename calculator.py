7540ac67b63b8abd4d1f0d0f98839c213f52d69e
Ethan <ethan@MacBook-Pro.hsd1.ca.comcast.net>
2021-03-02 20:50:53 -0800

    changed number 2 to number2 in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,16 @@
 def calculator(number1, number2, operator):
     if operator == '+':
-       return (number1 + number 2)
+       return (number1 + number2)
     elif operator == '-':
-       return (number1 - number 2)
+       return (number1 - number2)
     elif operator == '*':
-       return (number1 * number 2)
+       return (number1 * number2)
     elif operator == '/':
-       return (number1 / number 2)
+       return (number1 / number2)
     elif operator == '//':
-       return (number1 // number 2)
+       return (number1 // number2)
     elif operator == '**':
-       return (number1 ** number 2)
+       return (number1 ** number2)
     else:
        quit()
 
