54fe25cd0ade9584de45740428ceee6985f6ecc1
Ethan <ethan@MacBook-Pro.hsd1.ca.comcast.net>
2021-03-02 22:04:01 -0800

    fixed if statement

--- investment.py
+++ investment.py
@@ -1,5 +1,5 @@
 def calculate_apr(principal, interest_rate, years):
-    if interest_rate < 0
+    if interest_rate < 0:
        for i in range(years):
            total_value = (principal)*((1+interest_rate)**years)
            total_value = float(total_value)
