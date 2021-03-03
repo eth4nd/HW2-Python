3e8fc421dc4e9c47c8a9d741e5b473accf17788f
Ethan <ethan@MacBook-Pro.hsd1.ca.comcast.net>
2021-03-02 22:13:19 -0800

    changes to structure

--- investment.py
+++ investment.py
@@ -1,14 +1,10 @@
 def calculate_apr(principal, interest_rate, years):
     if interest_rate < 0:
+       return False
+    else:
        for i in range(years):
            total_value = (principal)*((1+interest_rate)**years)
            total_value = float(total_value)
-           output = isinstance(total_value, float)
-           if output:
-              return total_value
-           else:
-              return False
-    return False 
-    return total_value
+       return total_value
   
 calculate_apr(500, .03, 65)
