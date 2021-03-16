0c3e2cdcb33c1cd357460b3f3ba12a96f2624b4b
Ethan <ethan@MacBook-Pro.hsd1.ca.comcast.net>
2021-03-16 15:52:31 -0700

    moved input checks into calculate, and fixed return statements

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
 def calculator(number1, number2, operator):
     """
     A program that simulates like a calculator.
-    The operators allowed are +, -, *, /, **, and //, as
-    addition, subtraction, multiplication, division, integer division, 
-    and power, respectively  
+    The operators allowed are +, -, *, /, //, and **, as addition,
+    subtraction, multiplication, division, integer division, and
+    power, respectively
     """
     #This will only perform the operation that the input gives.
     if operator == '+':
@@ -13,31 +13,28 @@ def calculator(number1, number2, operator):
     elif operator == '*':
        return (number1 * number2)
     elif operator == '/':
-       return (number1 / number2)
+       if number2 == 0:
+          return False
+       else:
+          return (number1 / number2)
     elif operator == '//':
-       return (number1 // number2)
+       if number2 == 0:
+          return False
+       else:
+          return (number1 // number2)
     elif operator == '**':
        return (number1 ** number2)
     else:
-       quit()
+       return False
 
 def input_output():
     """
-    Takes input from the user and calls the calculator function. 
+    Takes input from the user and calls the calculator function.
     """
     number1 = input("Enter the first number: ")
     return (int(number1))
     number2 = input("Enter the second number: ")
-    if number2 == 0:
-       return false
-    else:
-       return (int(number2))
     operator = input("Enter the operation: ")
-    operatorlist = ['+', '-', '*', '/', '//', '**']
-    if operator in operatorlist:
-       return (operator)
-    else:
-       return false
     calculator(number1, number2, operator)
     continue_q = input("Do you wish to exit? ")
     return (continue_q)
